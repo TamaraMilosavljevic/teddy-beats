184650d8d0e9a9986006734180a52bce0a34bd6f
Tamara <mtamarajob@gmail.com>
2025-03-27 07:45:11 +0100

    My initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import React from "react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import ProtectedPage from "./ProtectedPage";
+import LoginPage from "./pages/LoginPage";
+import { Stack } from "@mui/material";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <Stack
+      sx={{
+        backgroundColor: (theme) => theme.palette.primary.bgColor,
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <SignedOut>
+        <LoginPage />
+      </SignedOut>
+      <SignedIn>
+        <ProtectedPage />
+      </SignedIn>
+    </Stack>
+  );
+};
 
 export default App;
